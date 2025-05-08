782fa39ec9018db6feafd5d550dcf14c112d5785
Ahmed Ibrahim Mohamed <ahmedibrahim.aim10@gmail.com>
2025-05-09 02:55:22 +0300

    Update main.py

--- model/main.py
+++ model/main.py
@@ -10,7 +10,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 
 
 def  get_clean():
-    data = pd.read_csv("C:/Users/Administrator/Downloads/archive (5)/data.csv")
+    data = pd.read_csv("data.csv")
   
     data = data.drop(['Unnamed: 32', 'id'], axis=1)
     data['diagnosis'] = data['diagnosis'].map({'M': 1 , 'B': 0})
